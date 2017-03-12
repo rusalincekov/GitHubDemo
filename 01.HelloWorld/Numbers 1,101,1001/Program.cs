c2caed5a0a5ccf23c836ae01920d646dc41a74ce
Rusalin Cekov <rusalin.cekov@gmail.com>
2017-03-12 16:05:19 +0200

    ops

--- 01.HelloWorld/Numbers 1,101,1001/Program.cs
+++ 01.HelloWorld/Numbers 1,101,1001/Program.cs
@@ -11,7 +11,7 @@ namespace Numbers_1_101_1001
         static void Main(string[] args)
         {
             Console.WriteLine("1");
-            Console.WriteLine("101");
+            Console.WriteLine("11");
             Console.WriteLine("1001");
         }
     }
