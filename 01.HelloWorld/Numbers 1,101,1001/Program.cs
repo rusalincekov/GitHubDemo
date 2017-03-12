a13d4a016ade77948d37fdca3f30b23b7f659844
Rusalin Cekov <rusalin.cekov@gmail.com>
2017-03-12 15:59:25 +0200

    solved number task

---
+++ 01.HelloWorld/Numbers 1,101,1001/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers_1_101_1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1");
+            Console.WriteLine("101");
+            Console.WriteLine("1001");
+        }
+    }
+}
